107cd611f2b0ede54da4d361c579c7495e017904
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-01 14:25:16 +0300

    Providing selectBook method with action with reduxt to react props (Bind Action Creator)

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'; //імпортували функцію для зєднання
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component{
     renderList(){
@@ -21,7 +23,7 @@ class BookList extends Component{
     }
 }
 
-//взаємодія react i redux
+//взаємодія react i redux для props
 function mapStateToProps(state){
     // все що повертається буде відображатись в props
     // всередині BookList
@@ -32,5 +34,13 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(BookList); 
+//взаємодія react i redux для функцій в actions (аяксів, тощо)
+function mapDispatchToProps(dispatch){
+    // коли selectBook викликана, результат буде прийшов до всіх наших reducers 
+    return bindActionCreators({selectBook:selectBook},dispatch);
+}
+
+//означає що до BookList компонента в props для state передається mapStateToProps
+//а action точніше всі ajax або тощо буде передаватись в props з mapDispatchToProps
+export default connect(mapStateToProps, mapDispatchToProps)(BookList); 
 //функція зєднує експортує функцію в контейнер
