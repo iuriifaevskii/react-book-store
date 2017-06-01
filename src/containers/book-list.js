06b7b4941a89a9daa2bf0bf9a7803497592b04a0
Iurii Faevskii <urfaevskii@gmail.com>
2017-06-01 13:23:03 +0300

    Making full connection data from redux to react container component and show it

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; //імпортували функцію для зєднання
 
-export default class BookList extends Component{
+class BookList extends Component{
     renderList(){
+        console.log(this.props.asdf); //-->123 // TEST
         return this.props.books.map((book)=>{
             return(
                 <li key={book.title} className="list-group-item">{book.title}</li>
@@ -10,6 +12,7 @@ export default class BookList extends Component{
     }
 
     render(){
+        console.log(this.props.asdf); //-->123 // TEST
         return(
             <ul className="list-group col-sm-4">
                 {this.renderList()}
@@ -17,3 +20,17 @@ export default class BookList extends Component{
         );
     }
 }
+
+//взаємодія react i redux
+function mapStateToProps(state){
+    // все що повертається буде відображатись в props
+    // всередині BookList
+    return {
+        asdf: '123', // TEST
+        books: state.books //в контейнері в елементі маєм ключ books і стейт букс це імпортуєм дані
+        //state змінюється і BookList rerender 
+    };
+}
+
+export default connect(mapStateToProps)(BookList); 
+//функція зєднує експортує функцію в контейнер
